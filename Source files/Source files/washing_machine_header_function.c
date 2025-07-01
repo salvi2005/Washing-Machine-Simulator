b6f53bdfc6cc3a5a9b5a8f051c38d011584d7389
Salvi Kashyap <kashyapsalvi575@gmail.com>
2025-07-01 10:01:22 +0530

    Create washing_machine_header_function.c

---
+++ Source files/Source files/washing_machine_header_function.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   washing_machine_header_function.c
+ * Author: Madhire Kalyan Sathwik
+ *
+ * Created on 11 June, 2025, 8:53 PM
+ */
+
+#ifndef WASHING_MACHINE_HEADER_FUNCTION_C
+#define	WASHING_MACHINE_HEADER_FUNCTION_C
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WASHING_MACHINE_HEADER_FUNCTION_C */
